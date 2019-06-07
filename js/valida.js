c6f408e6c5b8396438c8d0b0ccc796a7d6323a6d
Felipe Christiano Moeller Oliveira <felipe.oliveira_BOLS@pampa.compasso>
2019-06-07 14:41:41 -0300

    Atividade 4 - Desenvolvendo as validacoes

---
+++ js/valida.js
@@ -0,0 +1,10 @@
+var campo = $('.input-nome');
+
+function inicializaContadores() {
+    campo.on("input", function () {
+        var conteudo = campo.val();
+
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(qtdPalavras);
+    });
+}
